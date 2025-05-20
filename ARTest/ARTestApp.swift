0470d6a5da7a304a67e765621f73c10bc40d5f8a
milostuff <136461697+milostuff@users.noreply.github.com>
2025-05-20 15:58:19 +0800

    Initial Commit

---
+++ ARTest/ARTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARTestApp.swift
+//  ARTest
+//
+//  Created by Vian Martinez on 20/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ARTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
